53f4ca0316ff6643379f9e0b7c70a7e9debc101d
Renniej <tai.juan2@gmail.com>
2019-01-01 11:23:35 -0500

    Added Edit Profile Panel

--- controllers/AcctCreatorController.js
+++ controllers/AcctCreatorController.js
@@ -6,7 +6,6 @@ var urlencodedParser  = bodyParser.urlencoded({ extended: false });
 
 
 
-
 var getDate = function(){ //Grabbed from stackoverflow because... i didn't feel like coding it
     var today = new Date();
     var dd = today.getDate();
@@ -42,35 +41,38 @@ module.exports = function(app){
     app.post('/signup', urlencodedParser, function(req, res){
 
         //Vertify info here
-        var today = new Date();
 
-        var newUser = new User({
+      
 
-            Username : req.body.username,
-            Pass : req.body.password,
-            NickName : req.body.username,
-            Desc : 'New User',
-            Province : req.body.province,
-            Country : req.body.country,
-            DOB : req.body.date_of_birth,
-            Joined : getDate(),
-            Email : req.body.email,
-            Vertied : false
-            
+            var today = new Date();
 
-        })
+            var newUser = new User({
 
+                Username : req.body.username,
+                Pass : req.body.password,
+                NickName : req.body.username,
+                Desc : 'New User',
+                Province : req.body.province,
+                Country : req.body.country,
+                DOB : req.body.date_of_birth,
+                Joined : getDate(),
+                Email : req.body.email, 
+                Vertied : false //Did the user vertify their email/account
+                
 
-        newUser.save(function(err, data){
-            if (err){
-                throw err;
-            }
-            res.redirect('/profile/' + req.body.username);
-
-        });
+            })
 
 
-       
+            newUser.save(function(err, data){
+                if (err){
+                    throw err;
+                }
+                res.redirect('/profile/' + req.body.username);
+
+            });
+
+
+   
 
 
     });
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,85 +1,5 @@
-var Users  = [
+var Users = require('../models/user');
 
-    { id: 0, //User's ID number
-
-            info : {
-        Username : "Renniej",
-        Pass : "Test123",
-        Nick_Name : "Tai Rennie",
-        Desc : "HELLO! My name is rennie and i am attempting to learn a GIAGANTIC number of things on this platform. If you need me just send a message. k thanks.",
-        Province : "Ontario",
-        Country : "Canada",
-        DOB : "Feb 11 1999",
-        Joined : "October 2015",
-        Email : "testtest2@gmail.com",
-        Vertifed : true
-
-        },
-
-
-        Feed : [
-
-            {Content: "C++ is the best programming lang out there, hands down!", Date: "Jan 2015"},
-            {Content: "HELP IM FAILING EVERYTHING RIGHT NOW", Date: "Mar 2012"},
-            {Content: "Why do none of my professors speak english??????", Date: "Mar 2012"},
-            {Content: "The new spiderverse movie was SICKKKKKKKK", Date: "Mar 2012"},
-            {Content: "Guess who's the best!", Date: "Mar 2012"},
-            {Content: "Im so hungry q.q", Date: "April 2013"},
-            {Content: "Insert pointless quote here :)", Date: "Mar 2012"}
-        ]
-
-
-
-
-
-
-
-    }
-];
-
-
-
-
-
-
-var FindUser = function(elem,method){ //method = find user by which method (Username or ID)
-
-    var FoundUser = null;
-   
-
-    if (method ===  'Username'){ //If method specified is username then check the USERS database for matching username(elem)
-
-            for (i = 0; i < Users.length; ++i){
-
-                if (Users[i].info.Username == elem){
-
-                    FoundUser = Users[i];
-                    break;
-                }
-
-            }
-
-
-    }
-
-    else{
-
-        for (i = 0; i < Users.length; ++i){//If method specified is username then check the USERS database for matching ID(elem)
-
-            if (Users[i].id == elem){
-
-                FoundUser = Users[i];
-                break;
-            }
-
-        }
-
-    }
-    
-    return FoundUser;
-
-
-};
 
 
 module.exports = function(app){
@@ -87,19 +7,31 @@ module.exports = function(app){
     app.get('/profile/:id', function(req, res){
 
         
-        var FoundUser = FindUser(req.params.id,'Username');
+        Users.collection.findOne({Username : req.params.id}).then(function(result){
 
-        //console.log(FoundUser) 
+            if (result.Username === req.params.id){
 
-       if (FoundUser !== null){
-           res.status(200);
-             res.render('Profile', {User: FoundUser});
-        }
-        else{
+                res.status(200);
+    
+                //console.log(result);
+    
+                res.render('Profile', {User: result});
+            }
+      
+            
+
+        }).catch(function(err){
+
+
             res.status(404);
             res.send('USER DOES NOT EXIST... SORRY!');
-        }
-    
+
+
+        });
+
+
+
+        
     
         
     });
